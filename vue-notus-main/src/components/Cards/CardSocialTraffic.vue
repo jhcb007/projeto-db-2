67b09685997ffee5858f92e43133643c46fc1282
Henrique Brandao <jhcb007@gmail.com>
2021-08-05 23:05:21 -0300

    Inicio

--- vue-notus-main/src/components/Cards/CardSocialTraffic.vue
+++ vue-notus-main/src/components/Cards/CardSocialTraffic.vue
@@ -6,7 +6,7 @@
       <div class="flex flex-wrap items-center">
         <div class="relative w-full px-4 max-w-full flex-grow flex-1">
           <h3 class="font-semibold text-base text-blueGray-700">
-            Social traffic
+            Banco de Dados
           </h3>
         </div>
         <div
